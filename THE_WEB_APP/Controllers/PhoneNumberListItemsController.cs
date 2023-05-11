07b9592d2c043069ee84350606c703b218c82f88
Dell <Dell@DESKTOP-91D80O6>
2023-05-11 14:13:17 +0530

    Add project files.

---
+++ THE_WEB_APP/Controllers/PhoneNumberListItemsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using THE_WEB_APP.Models;
+
+namespace THE_WEB_APP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhoneNumberListItemsController : ControllerBase
+    {
+        private readonly PhoneNumberListContext _context;
+
+        public PhoneNumberListItemsController(PhoneNumberListContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/PhoneNumberListItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PhoneNumberListItem>>> GetPhoneNumberListItems()
+        {
+          if (_context.PhoneNumberListItems == null)
+          {
+              return NotFound();
+          }
+            return await _context.PhoneNumberListItems.ToListAsync();
+        }
+
+        // GET: api/PhoneNumberListItems/5
+        [HttpGet("id")]
+        public async Task<ActionResult<PhoneNumberListItem>> GetPhoneNumberListItem(int id)
+        {
+          if (_context.PhoneNumberListItems == null)
+          {
+              return NotFound();
+          }
+            var phoneNumberListItem = await _context.PhoneNumberListItems.FindAsync(id);
+
+            if (phoneNumberListItem == null)
+            {
+                return NotFound();
+            }
+
+            return phoneNumberListItem;
+        }
+
+        // PUT: api/PhoneNumberListItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPhoneNumberListItem(int id, PhoneNumberListItem phoneNumberListItem)
+        {
+            if (id != phoneNumberListItem.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(phoneNumberListItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhoneNumberListItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/PhoneNumberListItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<PhoneNumberListItem>> PostPhoneNumberListItem(PhoneNumberListItem phoneNumberListItem)
+        {
+          if (_context.PhoneNumberListItems == null)
+          {
+              return Problem("Entity set 'PhoneNumberListContext.PhoneNumberListItems'  is null.");
+          }
+            _context.PhoneNumberListItems.Add(phoneNumberListItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPhoneNumberListItem", new { id = phoneNumberListItem.ID }, phoneNumberListItem);
+        }
+
+        // DELETE: api/PhoneNumberListItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoneNumberListItem(int id)
+        {
+            if (_context.PhoneNumberListItems == null)
+            {
+                return NotFound();
+            }
+            var phoneNumberListItem = await _context.PhoneNumberListItems.FindAsync(id);
+            if (phoneNumberListItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.PhoneNumberListItems.Remove(phoneNumberListItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool PhoneNumberListItemExists(int id)
+        {
+            return (_context.PhoneNumberListItems?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ THE_WEB_APP/Migrations/20230509122248_Intial Migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace THE_WEB_APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class IntialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PhoneNumberListItems",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumberListItems", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PhoneNumberListItems");
+        }
+    }
+}
